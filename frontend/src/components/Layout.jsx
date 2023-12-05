76741ba52e862308bfbc6be4104f188e6c163756
Пользователь <husenrahmatov888@gmail.com>
2023-12-05 13:40:40 +0300

    husen

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";  
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div>
-        <Navbar />
-        {children}
-        <Footer />
+      <Navbar />
+      {children}
+      <Footer />
     </div>
-  )
+  );
 }

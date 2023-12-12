dc90bfa57bcb1c152ac95a9f57d3c81dbc2cf795
Faxriddin7 <secretnoname001@gmail.com>
2023-12-12 16:48:45 +0500

    confirm order

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
-
-
-
+import { Link } from 'react-router-dom';
+import ConfirmOrder from '../pages/ConfirmOrder';
 const Navbar = () => {
   return (
-    <nav className="flex font-bold  justify-evenly gap-5 items-center  bg-white py-4  ">
+    <nav className="flex font-bold justify-evenly gap-5 items-center bg-white py-4">
       <div>
-        <img className=  ' rounded-full w-[55px] h-[55px] ' src='/img/asia.jpg' alt="" />
+        <img className='rounded-full w-[55px] h-[55px]' src='/img/asia.jpg' alt="" />
       </div>
       <a href="" className="hover:text-orange-300 transition duration-300 text-black">О нас</a>
-      <a href="" className="hover:text-orange-300 transition duration-300  text-black">Меню</a>
-      <a href="" className="hover:text-orange-300 transition duration-300  text-black">Наши ценности</a>
-      <a href="" className="hover:text-orange-300 transition duration-300  text-black">Локации</a>
-      <a href="" className="hover:text-orange-300 transition duration-300  text-black">Карьера</a>
-      <a href="" className="hover:text-orange-300 transition duration-300  text-black">Кейтеринг</a>
-      <div className='flex gap-5 items-center '>
-        <a  href="#">
-        <img className=' rounded-full w-[20px] h-[20px] ' src="/img/search.png" alt="" />
+      <a href="" className="hover:text-orange-300 transition duration-300 text-black">Меню</a>
+      <Link to="/values" className="hover:text-orange-300 transition duration-300 text-black">
+        Наши ценности
+      </Link>
+      <a href="" className="hover:text-orange-300 transition duration-300 text-black">Локации</a>
+      <a href="" className="hover:text-orange-300 transition duration-300 text-black">Карьера</a>
+      <Link to="/keytering" className="hover:text-orange-300 transition duration-300 text-black">
+        Кейтеринг
+      </Link>
+      <div className='flex gap-5 items-center'>
+        <a href="#">
+          <img className='rounded-full w-[20px] h-[20px]' src="/img/search.png" alt="" />
         </a>
         <a href="">
-        <img className=' rounded-full w-[20px] h-[20px] ' src="/img/avatar.png" alt="" />
+          <img className='rounded-full w-[20px] h-[20px]' src="/img/avatar.png" alt="" />
         </a>
-        <a href="">
-        <img className=' rounded-full w-[20px] h-[20px] ' src="/img/cart.png" alt="" />
-        </a>
+        <Link to='/ConfirmOrder'>
+          <img className='rounded-full w-[20px] h-[20px]' src="/img/cart.png" alt="" />
+        </Link>
       </div>
     </nav>
   );
 };
+ 
 
 export default Navbar;
---
+++ frontend/src/pages/Keytering.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <img src="./img/backgroundimg.png" alt="" style={{ marginBottom: 'auto' }} /> 
+
+      <div className="w-[90%] mx-auto">
+      <div className="flex justify-between items-center">
+          <div className="w-[44%]">
+            <h1 className="text-[1.25rem]">НАША ИСТОРИЯ</h1>
+            <p className="text-[1.125rem] font-[300] my-[20px]">
+            История создания популярного производителя сладостей и выпечки, завоевавшего славу, благодаря мечте создать кондитерскую культуру в Узбекистане, и большой любви к общему, семейному делу.</p>
+        
+          </div>
+          <img
+            className="w-[480px] object-cover rounded-full h-[480px]"
+            loading="lazy"
+            src="https://safiabakery.uz/uploads/pages/page_1672066470.jpg"
+            alt=""
+          />
+        </div>
+
+        <div className="flex justify-between items-center my-28">
+          <img
+            className="w-[480px] object-cover rounded-full h-[480px]"
+            loading="lazy"
+            src="https://safiabakery.uz/uploads/pages/page_1672055055.jpg"
+            alt=""
+          />
+          <div className="w-[44%] text-right">
+            <h1 className="text-[1.25rem]">НАШИ ДОСТИЖЕНИЯ</h1>
+            <p className="text-[1.125rem] font-[300] my-[20px]">
+            Результат работы нашего коллектива отражает решения и стремления нашей команды в целом. В свою очередь, наши достижения являются стимулом для дальнейшего развития нашей работы.</p>
+           
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center mb-20">
+          <div className="w-[44%]">
+            <h1 className="text-[1.25rem]">SAFIA STORE</h1>
+            <p className="text-[1.125rem] font-[300] my-[20px]">
+            С целью быть еще ближе к нашимгостям мы запустили лимитированнуюколлекцию на нашей странице Safia Store. В нашей коллекции мы отразиличастичку индивидуальности длякаждого,
+чтобы вы стали частью нашейбольшой и дружной семьи! </p>
+            </div>
+          <img
+            className="w-[480px] object-cover rounded-full h-[480px]"
+            loading="lazy"
+            src="https://safiabakery.uz/uploads/pages/page_1671111750.png"
+            alt=""
+          />
+        </div>
+      </div>
+        </div> 
+        
+  );
+}
+
+export default App;
